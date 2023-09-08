9abe5bb63a73540d88d51de29d46f77223fd3759
Lostceleste <inymtern_cl@126.com>
2023-09-08 23:09:34 +0800

    bg update

--- src/js/api/commonApi.js
+++ src/js/api/commonApi.js
@@ -1,8 +1,12 @@
 
 import axios from "../request";
-
-
+import { userStore } from '../../stores/Store'
+const store = userStore()
 export function uploadImg(file) {
+    if(!store.isLogin) {
+        window.toastr.error('请登录后重试')
+        return 
+    }
     return axios.post('/obs/images/upload', file, {
         headers: {
             'Content-Type': 'multipart/form-data'
